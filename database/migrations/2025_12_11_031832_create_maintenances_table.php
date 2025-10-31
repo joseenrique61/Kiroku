ff6a6d5da58d5f4484cfb068c1e2a46ac263a715
Arieloou <ariel.anchapaxi@gmail.com>
2025-10-30 22:41:18 -0500

    Created new migration definitions for all the tables in the app

---
+++ database/migrations/2025_12_11_031832_create_maintenances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maintenances', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_preventive')->default(true);
+            $table->decimal('cost');
+            $table->timestampTz('datetime');
+            $table->foreignId('device_id')->constrained('devices');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maintenances');
+    }
+};
