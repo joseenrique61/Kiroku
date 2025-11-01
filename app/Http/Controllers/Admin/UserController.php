b9295a159ac1d10c875c9212fbed6d5e937f75b3
Arieloou <ariel.anchapaxi@gmail.com>
2025-11-01 15:57:52 -0500

    Added vies, routes and UserController to admin the app

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Inertia\Inertia;
+use Inertia\Response;
+use Spatie\Permission\Models\Role;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        $users = User::with('role')->get();
+        
+        return Inertia::render('admin/users/index', [
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        $roles = Role::all();
+
+        return Inertia::render('admin/users/create', [
+            'roles' => $roles
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if (empty($request->role))
+        {
+            $user->syncRoles('guest');
+        }
+
+        $user->syncRoles($request->role);
+
+        return redirect()->intended(route('users', absolute: false));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user) : Response
+    {
+        return Inertia::render('admin/users/show',[
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user) : Response
+    {
+        $roles = Role::all();
+
+        return Inertia::render('admin/users/edit',[
+            'user' => $user,
+            'roles' => $roles
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user) : RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'password' => ['nullable', 'confirmed', Rules\Password::defaults()],
+            'role' => 'required|string|exists:roles,name'
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+        ]);
+
+        if ($request->filled('password')) {
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+        }
+            
+        $user->syncRoles($request->role);
+
+        return redirect()->intended(route('users', absolute: false));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user) : RedirectResponse
+    {
+        $user->delete();
+        return redirect()->intended(route('users', absolute: false))->with('success','User was deleted successfully!');
+    }
+}
