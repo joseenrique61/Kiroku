c6e766f75f2d2235bc6e7c5641d2ac2e64fca221
Arieloou <ariel.anchapaxi@gmail.com>
2025-11-01 18:29:50 -0500

    Created new controllers for specific models

---
+++ app/Http/Controllers/Inventory/Failures/FailureTypeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FailureType;
+use Illuminate\Http\Request;
+
+class FailureTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FailureType $failureType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FailureType $failureType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FailureType $failureType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FailureType $failureType)
+    {
+        //
+    }
+}
