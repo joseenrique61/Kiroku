9fbb5ab540ff1a83ff38ff1fa912ec6ae3ce2433
Arieloou <ariel.anchapaxi@gmail.com>
2025-11-04 18:53:04 -0500

    Components with old reference errors were corrected

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import AppLayout from '@/layouts/app-layout';
+// import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
