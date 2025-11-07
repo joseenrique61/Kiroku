fe2448702e5e151abc7ce1241a134eb9321e3d2c
Arieloou <ariel.anchapaxi@gmail.com>
2025-11-06 23:36:32 -0500

    Created new Seeder to Users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $userAdmin = User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $userAdmin->syncRoles('admin');
     }
 }
