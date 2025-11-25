2101dd2d75784a33950895b43784a4f873a5af43
Arieloou <ariel.anchapaxi@gmail.com>
2025-11-25 18:03:24 -0500

    Improved logic to process analytics

--- database/factories/MaintenanceFactory.php
+++ database/factories/MaintenanceFactory.php
@@ -18,6 +18,7 @@ class MaintenanceFactory extends Factory
     public function definition(): array
     {
         $outOfServiceDateTime = $this->faker->dateTimeThisYear();
+        $backToServiceDateTime = $this->faker->dateTimeThisYear();
         $maintenanceDateTime = $this->faker->dateTimeBetween(
             $outOfServiceDateTime->format('Y-m-d'),
             'now'
@@ -28,10 +29,20 @@ class MaintenanceFactory extends Factory
             $maintenanceDateTime->modify('+1 day');
         }
 
+        // Ensure maintenanceDateTime is strictly after outOfServiceDateTime
+        if ($backToServiceDateTime->format('Y-m-d') === $maintenanceDateTime->format('Y-m-d')) {
+            $backToServiceDateTime->modify('+10 day');
+        }
+
         // Ensure maintenanceDateTime does not exceed the end of the current year
         $endOfYear = (new \DateTime('last day of December this year'))->setTime(23, 59, 59);
         if ($maintenanceDateTime > $endOfYear) {
             $maintenanceDateTime = $endOfYear;
+        }
+        
+        // Ensure backToServiceDatetime does not exceed the end of the current year
+        if ($backToServiceDateTime > $endOfYear) {
+            $backToServiceDateTime = $endOfYear;
         }
 
         return [
@@ -39,6 +50,7 @@ class MaintenanceFactory extends Factory
             'cost' => $this->faker->randomFloat(2, 10, 1000),
             'datetime' => $maintenanceDateTime->format('Y-m-d'),
             'out_of_service_datetime' => $outOfServiceDateTime->format('Y-m-d'),
+            'back_to_service_datetime' => $backToServiceDateTime->format('Y-m-d'),
             'device_id' => Device::factory(),
         ];
     }
